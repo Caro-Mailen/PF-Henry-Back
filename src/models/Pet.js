65ca630f7e54b2146ed467734ef7c18a81623ed0
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-21 20:23:39 -0300

    first Models

--- src/models/Pet.js
+++ src/models/Pet.js
@@ -6,9 +6,41 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    race: {
+    image: {
       type: DataTypes.STRING,
+      defaultValue: "https://baltrion.es/wp-content/uploads/sin-IMAGEN.jpg",
+    },
+    size: {
+      type: DataTypes.ENUM(["small","medium", "big"]),
       allowNull: false,
+      defaultValue: "medium",
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+    },
+    fur: {
+      type: DataTypes.ENUM(["short","medium", "large"]),
+      allowNull: false,
+      defaultValue: "medium",
+    },
+    breed: {
+      type: DataTypes.ENUM(["crossbreed", "salchicha", "bulldog"]),
+      allowNull: false,
+      defaultValue: "crossbreed",
+    },
+    gender: {
+      type: DataTypes.ENUM(["female","male"]),
+      allowNull: false
+    },
+    castration: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    vaccinate: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
     }
   });
 };
