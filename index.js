3fe458903532b62458f1fa19d651c44a8deba89a
Caro-Mailen <caro.guzman21@outlook.es>
2022-06-21 12:15:47 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const Express = require("express");
+
+const app = Express();
+
+app.get("/", (req, res) => {
+  res.send("hola grupo");
+});
+
+app.listen(3000, () => {
+  console.log("server listen at port 3000");
+});
