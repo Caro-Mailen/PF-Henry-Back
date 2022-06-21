2fc8e5f15d149bd1353c649000cec533683401a8
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-21 20:45:18 -0300

    fixingg

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const server = require('./src/app.js');
 const { db } = require('./src/db.js');
 const { pet, type } = require('./src/db');
 
-
-<<<<<<< HEAD
 db.sync({ force: true }).then(() => {
   server.listen(3001, async () => {
     console.log('%s Server Levantado: 3001');
@@ -19,14 +17,4 @@ db.sync({ force: true }).then(() => {
     //   vaccinate: true
     // })
   });
-=======
-app.get("/", (req, res) => {
-  res.send("hola grupo");
-});
-
-//intentando cambiar algo
-
-app.listen(3000, () => {
-  console.log("server listen at port 3000");
->>>>>>> 8bb9252ca27828b0b98f5de6b261e6f4f41509d0
 });
