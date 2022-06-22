7345fadc35bb3a7fa001b7dd226b7134127dcc5e
Caro-Mailen <caro.guzman21@outlook.es>
2022-06-22 13:13:55 -0300

    load DB

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const server = require('./src/app.js');
-const { db } = require('./src/db.js');
-// const { Pet, Type } = require('./src/db');
+const server = require("./src/app.js");
+const { db } = require("./src/db.js");
+const { Pet, Type } = require("./src/db");
+const json = require("./src/mascotas.json");
+// const Pet = require("./src/models/Pet.js");
+// console.log(json);
+
+const mascotas = json.data;
+// console.log(mascotas);
 
 db.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
-    console.log('%s Server Levantado: 3001');
+    console.log("%s Server Levantado: 3001");
+    await Pet.bulkCreate(mascotas);
     // await Pet.create({
     //   name: "tristan",
     //   image: "https://i.postimg.cc/1tKP9NkV/tristan.jpg",
