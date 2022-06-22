0439d32dc102f0104e452393b322c2cdeadcf051
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-22 11:03:38 -0300

    el define de los modelos ahora empieza con mayus

--- src/models/Pet.js
+++ src/models/Pet.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('pet', {
+  sequelize.define('Pet', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
