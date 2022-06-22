b70fba3f62547d933beb649aa70c03a87c7a9daa
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-22 11:17:07 -0300

    fix get

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const { pet, type } = require('../db.js');
+const { Pet, Type } = require('../db.js');
 
 const router = Router();
 
 router.get('/',(req, res) => {
-    pet.findAll().then(r=>res.send(r));
+    Pet.findAll().then(r=>res.send(r));
 })
 
 module.exports = router;
