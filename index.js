1c5b37720522d15603279389c7c167d314e078f5
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-22 13:21:43 -0300

    eliminar hardcodeo

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const server = require("./src/app.js");
 const { db } = require("./src/db.js");
-const { Pet, Type } = require("./src/db");
-const json = require("./src/mascotas.json");
-// const Pet = require("./src/models/Pet.js");
-// console.log(json);
+const { Pet } = require("./src/db");
+const json = require("./src/mascotas.json"); //cuando lucas suba las carpetas ordenadas cambiar.
 
 const mascotas = json.data;
-// console.log(mascotas);
 
 db.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log("%s Server Levantado: 3001");
     await Pet.bulkCreate(mascotas);
-    // await Pet.create({
-    //   name: "tristan",
-    //   image: "https://i.postimg.cc/1tKP9NkV/tristan.jpg",
-    //   size: "big",
-    //   weight: "10",
-    //   fur: "short",
-    //   breed: "crossbreed",
-    //   gender: "male",
-    //   castration: true,
-    //   vaccinate: true
-    // })
   });
 });
