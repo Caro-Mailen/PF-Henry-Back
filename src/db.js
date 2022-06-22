78cd8983c31eca784afeeea4b881732ee3104005
unknown <ignaciojsolaz@gmail.com>
2022-06-21 21:01:02 -0300

    second commit

--- src/db.js
+++ src/db.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const Pet = require('./models/Pet.js')
-const User = require('./models/User.js')
+const User = require('./models/User.js');
+const Donation = require('./models/Donation.js');
 
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
@@ -15,13 +16,19 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
 try{
   Pet(sequelize);
   User(sequelize);
+  Donation(sequelize);
 } catch (e) {
   console.log(e.message);
 }
 
 let {pet, user, donation} = sequelize.models;
 
+
 //relaciones.
+user.hasMany(pet);
+pet.belongsTo(user);
+user.hasMany(donation);
+donation.belongsTo(user);
 
 module.exports = {
   ...sequelize.models,
