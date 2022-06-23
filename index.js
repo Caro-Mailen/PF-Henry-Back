674ba1e377c8c3f44b311d0459636c982b4f8aa5
Caro-Mailen <caro.guzman21@outlook.es>
2022-06-23 16:23:47 -0300

    Co-authored-by: Lucas Giorgi <lucasgiorgi95@users.noreply.github.com> Co-authored-by: Agustin Di giacinto <AgustinDi@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const server = require("./src/app.js");
 const { db } = require("./src/db.js");
-const { Pet } = require("./src/db");
-const json = require("./src/Helper/mascotas.json"); 
+const { Pet, User } = require("./src/db");
+const json = require("./src/Helper/mascotas.json");
+const Ujson = require("./src/Helper/users.json");
 
 const mascotas = json.data;
+const users = Ujson.data;
 
 db.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log("%s Server Levantado: 3001");
     await Pet.bulkCreate(mascotas);
+    await User.bulkCreate(users);
   });
 });
