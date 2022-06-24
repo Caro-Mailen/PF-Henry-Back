f1f67e626d3f1726b0238cc358fcda38fcec1a02
Caro-Mailen <caro.guzman21@outlook.es>
2022-06-24 16:33:18 -0300

    filtros gender y sizze

--- src/Controllers/controllerPets.js
+++ src/Controllers/controllerPets.js
@@ -1,74 +1,74 @@
 //aca van las funciones controladoras de las rutas pets
 const { Pet, User } = require("../db.js");
 
-  function petName (req, res, next) {
-    const { name } = req.query;
-    if(!name)return next()
-    Pet.findOne({ where: { name: name } }).then((r) => res.send(r));
+function petName(req, res, next) {
+  const { name } = req.query;
+  if (!name) return next();
+  Pet.findOne({ where: { name: name } }).then((r) => res.send(r));
 }
 
-const pet = async (req, res)=> {
-    const { page = 0, size = 6 } = req.query;
+const pet = async (req, res) => {
+  const { page = 0, size = 6, sizePet, gender } = req.query;
 
-    const options = {
-      limit: +size,
-      offset: (+page) * (+size),
-    };
-  
-    const { count, rows } = await Pet.findAndCountAll(options);
-  
-    res.json({
-      total: count,
-      pets: rows,
-    });
-  
-    // Pet.findAll().then((r) => res.send(r));
-  }
+  const obj = {};
 
+  if (sizePet) obj.size = sizePet;
+  if (gender) obj.gender = gender;
 
-function petId (req, res) {
-    const { id } = req.params;
-    Pet.findByPk(id).then((r) => res.send(r));
+  const options = {
+    limit: size,
+    offset: size * page,
+  };
+  if (obj.size || obj.gender) options.where = obj;
+
+  const petFind = await Pet.findAll(options);
+
+  return res.json(petFind);
+};
+
+function petId(req, res) {
+  const { id } = req.params;
+  Pet.findByPk(id).then((r) => res.send(r));
 }
 
 const petPost = async (req, res) => {
-      const {
-        name,
-        image,
-        size,
-        weight,
-        fur,
-        breed,
-        gender,
-        castration,
-        vaccinate,
-      } = req.body;
-    
-      if (
-        !name ||
-        !image ||
-        !size ||
-        !fur ||
-        !breed ||
-        !gender ||
-        !castration ||
-        !vaccinate
-      )
-        return res.status(400).send("please insert require fields to continue");
-    
-      try {
-        let infoPet = { ...req.body };
-        const newPet = await Pet.create(infoPet);
-        // console.log(newPet);
-        res.status(200).send(newPet);
-      } catch (error) {
-        console.log(error);
-      }
-    }
+  const {
+    name,
+    image,
+    size,
+    weight,
+    fur,
+    breed,
+    gender,
+    castration,
+    vaccinate,
+  } = req.body;
 
-module.exports={
-    pet,
-    petId,
-    petName,
-    petPost
+  if (
+    !name ||
+    !image ||
+    !size ||
+    !fur ||
+    !breed ||
+    !gender ||
+    !castration ||
+    !vaccinate
+  )
+    return res.status(400).send("please insert require fields to continue");
+
+  try {
+    let infoPet = { ...req.body };
+    const newPet = await Pet.create(infoPet);
+    // console.log(newPet);
+    res.status(200).send(newPet);
+  } catch (error) {
+    console.log(error);
   }
+};
+
+module.exports = {
+  pet,
+  petId,
+  petName,
+  petPost,
+};
