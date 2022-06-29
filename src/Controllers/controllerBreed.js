7b075c5cbeb2dbd71ef86f39e31767a44e6894f6
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-06-28 21:37:35 -0300

    breed

--- src/Controllers/controllerBreed.js
+++ src/Controllers/controllerBreed.js
@@ -2,12 +2,17 @@
 const { Pet } = require('../db.js')
 
 const breed = async (req, res, next) => {
-  const pets = await Pet.findAll()
-  const breeds = new Set()
-  pets.forEach(pet => {
-    breeds.add(pet.breed)
-  })
-  res.send(Array.from(breeds))
+  const { pet } = req.query
+  try {
+    const pets = await Pet.findAll(pet ? { where: { pet } } : {})
+    const breeds = new Set()
+    pets.forEach(pet => {
+      breeds.add(pet.breed)
+    })
+    res.send(Array.from(breeds))
+  } catch (e) {
+    res.status(404).send('pet value can only be dog or cat')
+  }
 }
 
 module.exports = {
