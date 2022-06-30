23e0f352864e3b821cbec6f8c49b618452277431
ynavarro44 <yinanavarro44>
2022-06-30 10:35:43 -0500

    Act

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,11 +4,8 @@ const pet = require('./routePets')
 const user = require('./routeUser')
 const userPet = require('./ruteUserPet')
 const donation = require('./ruteDonation')
-<<<<<<< HEAD
 const payment = require('./routePayment')
-=======
 const breed = require('./routeBreed.js')
->>>>>>> f97ed87555b155a3141029cc8eead891c6694d4e
 
 // const user = require('./routeUser')
 
@@ -18,11 +15,8 @@ router.use('/pet', pet)
 router.use('/donation', donation)
 router.use('/user', user)
 router.use('/userPet', userPet)
-<<<<<<< HEAD
 router.use('/payment', payment)
-=======
 router.use('/breed', breed)
 router.use('/', (req, res, next) => res.send('welcome to pfmascotas-api'))
->>>>>>> f97ed87555b155a3141029cc8eead891c6694d4e
 
 module.exports = router
