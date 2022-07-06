a146aa351c37efb33b9ceae2f9f1b43264847bbc
Caro-Mailen <caro.guzman21@outlook.es>
2022-07-06 17:08:07 -0300

    template de bienvenida para emails

---
+++ src/Helper/templateWelcome.js
@@ -0,0 +1,132 @@
+const emailWelcome = ` <body style="width:100%;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
+<div class="es-wrapper-color" style="background-color:#FFFFFF"><!--[if gte mso 9]>
+    <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+      <v:fill type="tile" color="#ffffff"></v:fill>
+    </v:background>
+  <![endif]-->
+ <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top">
+   <tr style="border-collapse:collapse">
+    <td valign="top" style="padding:0;Margin:0">
+     <table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
+       <tr style="border-collapse:collapse">
+        <td class="es-adaptive" style="padding:0;Margin:0;background-color:transparent" bgcolor="transparent" align="center">
+         <table class="es-content-body" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:transparent;width:600px" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+           <tr style="border-collapse:collapse">
+            <td align="left" style="padding:10px;Margin:0"><!--[if mso]><table style="width:580px"><tr><td style="width:280px" valign="top"><![endif]-->
+             <table class="es-left" cellspacing="0" cellpadding="0" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left">
+               <tr style="border-collapse:collapse">
+                <td align="left" style="padding:0;Margin:0;width:280px">
+                 <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                   <tr style="border-collapse:collapse">
+                    <td class="es-infoblock es-m-txt-c" align="left" style="padding:0;Margin:0;line-height:14px;font-size:12px;color:#CCCCCC"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:14px;color:#CCCCCC;font-size:12px">Put your preheader text here</p></td>
+                   </tr>
+                 </table></td>
+               </tr>
+             </table><!--[if mso]></td><td style="width:20px"></td><td style="width:280px" valign="top"><![endif]-->
+             <table class="es-right" cellspacing="0" cellpadding="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:right">
+               <tr style="border-collapse:collapse">
+                <td align="left" style="padding:0;Margin:0;width:280px">
+                 <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                   <tr style="border-collapse:collapse">
+                    <td align="right" class="es-infoblock es-m-txt-c" style="padding:0;Margin:0;line-height:14px;font-size:12px;color:#CCCCCC"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:14px;color:#CCCCCC;font-size:12px"><a class="view" href="https://viewstripo.email" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#CCCCCC;font-size:12px">View in browser</a></p></td>
+                   </tr>
+                 </table></td>
+               </tr>
+             </table><!--[if mso]></td></tr></table><![endif]--></td>
+           </tr>
+         </table></td>
+       </tr>
+     </table>
+     <table class="es-header" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top">
+       <tr style="border-collapse:collapse">
+        <td class="es-adaptive" style="padding:0;Margin:0;background-color:#205287" bgcolor="#205287" align="center">
+         <table class="es-header-body" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:transparent;width:600px" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center">
+           <tr style="border-collapse:collapse">
+            <td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px">
+             <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+               <tr style="border-collapse:collapse">
+                <td align="center" valign="top" style="padding:0;Margin:0;width:560px">
+                 <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;border-left:1px solid transparent" role="presentation">
+                   <tr style="border-collapse:collapse">
+                    <td align="center" style="padding:0;Margin:0;font-size:0px"><img class="adapt-img" src="https://jltgun.stripocdn.email/content/guids/videoImgGuid/images/whatsapp_image_20220705_at_70015_pm_1.jpeg" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="220"></td>
+                   </tr>
+                 </table></td>
+               </tr>
+             </table></td>
+           </tr>
+         </table></td>
+       </tr>
+     </table>
+     <table class="es-content" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
+       <tr style="border-collapse:collapse">
+        <td style="padding:0;Margin:0;background-color:#062c46" bgcolor="#062c46" align="center">
+         <table class="es-content-body" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:transparent;border-top:2px solid transparent;border-right:2px solid transparent;border-left:2px solid transparent;width:600px;border-bottom:2px solid transparent" cellspacing="0" cellpadding="0" bgcolor="transparent" align="center">
+           <tr style="border-collapse:collapse">
+            <td style="Margin:0;padding-bottom:5px;padding-left:20px;padding-right:20px;padding-top:25px;background-position:right bottom" align="left">
+             <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+               <tr style="border-collapse:collapse">
+                <td valign="top" align="center" style="padding:0;Margin:0;width:556px">
+                 <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                   <tr style="border-collapse:collapse">
+                    <td align="center" style="padding:0;Margin:0;padding-top:20px;padding-bottom:25px"><h1 style="Margin:0;line-height:41px;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;font-size:34px;font-style:normal;font-weight:normal;color:#cfe2f3">Bienvenido!</h1></td>
+                   </tr>
+                   <tr style="border-collapse:collapse">
+                    <td class="es-m-txt-c" align="center" style="padding:0;Margin:0;padding-top:10px;padding-bottom:35px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:24px;color:#ffffff;font-size:16px">Estamos&nbsp;felices de que estés aquí.<br>Somos una comunidad apasionada por los animales&nbsp;</p></td>
+                   </tr>
+                   <tr style="border-collapse:collapse">
+                    <td class="es-m-txt-c" align="center" style="padding:0;Margin:0;padding-bottom:35px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:24px;color:#ead1dc;font-size:16px">"Hasta que no hayas amado a un animal,<br>una parte de tu alma permanecera dormida."<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -Anatole France-</p></td>
+                   </tr>
+                   <tr style="border-collapse:collapse">
+                    <td align="center" style="padding:0;Margin:0;padding-left:10px;padding-right:10px"><span class="es-button-border" style="border-style:solid;border-color:transparent;background:#1376c8;border-width:1px;display:inline-block;border-radius:5px;width:auto"><a href="https://pf-henry-front-three.vercel.app/" class="es-button" target="_blank" style="mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#ffffff;font-size:18px;border-style:solid;border-color:#1376c8;border-width:10px 20px 10px 20px;display:inline-block;background:none 0% 0% repeat scroll #1376c8;border-radius:5px;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;font-weight:normal;font-style:normal;line-height:22px;width:auto;text-align:center">Ingresa a nuestra web</a></span></td>
+                   </tr>
+                 </table></td>
+               </tr>
+             </table></td>
+           </tr>
+           <tr style="border-collapse:collapse">
+            <td style="padding:0;Margin:0;padding-left:20px;padding-right:20px;background-position:right bottom" align="left">
+             <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+               <tr style="border-collapse:collapse">
+                <td valign="top" align="center" style="padding:0;Margin:0;width:556px">
+                 <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                   <tr style="border-collapse:collapse">
+                    <td align="center" style="padding:0;Margin:0;font-size:0px"><img class="adapt-img" src="https://www.unitedpetfood.es/blog/wp-content/uploads/2017/08/mascotas-perros-gatos-conejos.jpg" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="556"></td>
+                   </tr>
+                 </table></td>
+               </tr>
+             </table></td>
+           </tr>
+         </table></td>
+       </tr>
+     </table>
+     <table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%">
+       <tr style="border-collapse:collapse">
+        <td align="center" bgcolor="#205287" style="padding:0;Margin:0;background-color:#205287">
+         <table class="es-content-body" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:transparent;width:600px">
+           <tr style="border-collapse:collapse">
+            <td style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:20px;padding-right:20px;background-position:center bottom" align="left">
+             <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+               <tr style="border-collapse:collapse">
+                <td valign="top" align="center" style="padding:0;Margin:0;width:560px">
+                 <table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                   <tr style="border-collapse:collapse">
+                    <td align="center" class="es-infoblock" style="padding:0;Margin:0;line-height:14px;font-size:12px;color:#CCCCCC"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:14px;color:#CCCCCC;font-size:12px">You have received this email as a registered user of pf-henry-front-three.vercel.app/</p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:14px;color:#CCCCCC;font-size:12px"><br></p></td>
+                   </tr>
+                   <tr style="border-collapse:collapse">
+                    <td class="es-infoblock" align="center" style="padding:0;Margin:0;padding-top:10px;line-height:14px;font-size:12px;color:#CCCCCC"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:'trebuchet ms', 'lucida grande', 'lucida sans unicode', 'lucida sans', tahoma, sans-serif;line-height:14px;color:#CCCCCC;font-size:12px">© AdoptA, Inc.</p></td>
+                   </tr>
+                 </table></td>
+               </tr>
+             </table></td>
+           </tr>
+         </table></td>
+       </tr>
+     </table></td>
+   </tr>
+ </table>
+</div>
+</body>`
+
+module.exports = {
+  emailWelcome
+}
