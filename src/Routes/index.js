361b9f5a625f5e897af0fd6a7d4e172ad05e692d
Agustin Di giacinto <88413954+AgustinDi@users.noreply.github.com>
2022-07-09 04:30:59 -0300

    routes no working

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,6 +7,7 @@ const payment = require('./routePayment')
 const breed = require('./routeBreed.js')
 const petitionGet = require('./routePetitionGet.js')
 const petitionLoad = require('./routePetitionLoad.js')
+const admin = require('./routeAdmin.js')
 // const passport = require('passport')
 
 // const user = require('./routeUser')
@@ -21,6 +22,7 @@ router.use('/payment', payment)
 router.use('/breed', breed)
 router.use('/petitionGet', petitionGet)
 router.use('/petitionLoad', petitionLoad)
+router.use('/admin', admin)
 router.use('/', (req, res, next) => res.send('welcome to pfmascotas-api'))
 
 module.exports = router
